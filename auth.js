f151598b5855fb2b8960ab19c4fae702849171a2
Eerik <2392230s@student.gla.ac.uk>
2020-08-17 10:14:27 +0100

    Heroku

---
+++ auth.js
@@ -0,0 +1,4 @@
+exports.genius_client = 'Zzwi4b4RdUPhmTgBL0vAXW-Dvmn032dJgm3UbJ9oV6ghvGbiEwcLRVls53NQXiaQ'
+exports.genius_secret = 'xFZmHk_Ek_Ew46DqbvRjNEFQZo43N0Go8BJazbo7rWNpi-dTN2uz_8DY_ADMJifCNbdCYwGxqDglbboYE5Tm0w'
+exports.google_client = 'AIzaSyDzgkV6BCYQYEKURTZGmpu4Yh5WBkJqQ_s'
+exports.google_authenticator_client = '359548864121-f3blhpvvgm17oqoun8tvh2708a8loujm.apps.googleusercontent.com'
